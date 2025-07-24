564b9cc3a8718523305f1d60fb6f851ce72ce8a2
Olray <olray@allanime.org>
2025-07-24 12:09:02 +0200

    Added test for TMemIniFile compatibility

--- UnixStreamWriter/Demo.dpr
+++ UnixStreamWriter/Demo.dpr
@@ -1,4 +1,4 @@
-program Demo;
+﻿program Demo;
 
 {$APPTYPE CONSOLE}
 
@@ -6,10 +6,13 @@ program Demo;
 
 uses
   System.SysUtils,
+  System.Classes,
+  System.IniFiles,
   UnixStreamWriter in 'UnixStreamWriter.pas';
 
 var
-  Writer: TUnixStreamWriter;
+  Writer: TStreamWriter;
+  IniFile: TMemIniFile;
 begin
   try
       // TUnixStreamWriter modifies the line break from sLineBreak to #10
@@ -23,6 +26,27 @@ begin
     Writer.WriteLine('without BOM and');
     Writer.WriteLine('with Unix line breaks');
     Writer.Free;
+
+      // from https://en.delphipraxis.net/topic/2476-skipping-the-utf-8-bom-with-tmeminifile-in-delphi-2007/
+      // test if TMemIniFile can read BOM-less ini files
+
+      // write some ini file (no BOM, windows line breaks, e.g. Visual Studio Code)
+    Writer := TStreamWriter.Create(
+        'test.ini',
+        False,
+        TEncoding.UTF8NoBOM);
+    Writer.WriteLine('[Values]');
+    Writer.WriteLine('Value=some German umlauts: öäüß');
+    Writer.Free;
+
+      // read into TIniFile
+    IniFile := TMemIniFile.Create('test.ini', TEncoding.UTF8);
+    var StringValue := IniFile.ReadString('Values', 'Value', '');
+    if(StringValue <> 'some German umlauts: öäüß') then
+      raise Exception.Create('TMemIniFile unexpectedly didn''t read the BOM-less file as utf8');
+      // yes it can
+    IniFile.Free;
+
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
